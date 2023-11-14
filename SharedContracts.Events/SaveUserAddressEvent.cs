71f1277b6084ee86d0711d19bc11a10d4e5ee878
mateus <imw.desenv@gmail.com>
2023-11-14 20:41:31 -0300

    feat: Add contract to send message using rabbitMq

---
+++ SharedContracts.Events/SaveUserAddressEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedContracts.Events
+{
+    public class SaveUserAddressEvent : BaseUnivanEvent
+    {
+        public int UserId { get; set; }
+        public String PlaceId { get; set; }
+        public int? RelatedTo { get; set; }
+    }
+}
